ad991020baa760daa8db0a6ba6168f4d7042c818
Phillip <proberts3@gatech.edu>
2024-08-17 10:16:41 -0600

    Add project files.

---
+++ GnomURL.Server/Controllers/URLGeneratorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace GnomURL.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class URLGeneratorController : ControllerBase
+    {
+        private readonly ILogger<URLGeneratorController> _logger;
+
+        public URLGeneratorController(ILogger<URLGeneratorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "PostGenerateURL")]
+        public string Post()
+        {
+            return "";
+        }
+    }
+}
